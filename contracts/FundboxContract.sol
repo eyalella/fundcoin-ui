d24891e9966cbe3d0964f8e0bd84bbca8428af11
br0s <dima@fundbox.com>
2017-10-26 02:35:36 +0300

    FundboxContract [WIP]

--- contracts/FundboxContract.sol
+++ contracts/FundboxContract.sol
@@ -1,7 +1,39 @@
 pragma solidity ^0.4.11;
 
+import 'contracts/TrackingBasicToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract FundboxContract {
+contract FundboxContract is TrackingBasicToken {
+
+    //########## FUNDCOIN ###########
+    string public name = 'FundCoin';
+    string public symbol = 'FC';
+    uint public decimals = 0;
+    uint public INITIAL_SUPPLY = 100;
+    uint public PRICE = 1000;
+    address owner_addr;
+
+    function FundboxContract(){
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        owner_addr = msg.sender;
+    }
+
+    function investInFundCoin() public payable {
+        transferFrom(owner_addr, msg.sender, SafeMath.div(x, PRICE));
+    }
+
+    function payOut(uint fees) {
+        uint payout_per_coin = SafeMath.div(fees, INITIAL_SUPPLY);
+        for (uint i = 0; i < coin_owners.length; i++) {
+            if (coin_owners[i] > 0) {
+                coin_owners[i].transfer(SafeMath.mul(balances[coin_owners[i]], payout_per_coin));
+            }
+        }
+    }
+
+
+    //########## FUNDBOX CONTRACT ###########
 
     enum LoanRequestStatus { Accepted, Rejected, Pending }
 

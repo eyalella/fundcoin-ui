07732bdb0e292f0d94e9e8e9a28603fc9c2ee8d9
Eyal Ella <eyal.ella@fundbox.com>
2017-10-25 15:52:22 +0300

    adding a new contract

--- src/user/ui/loginbutton/LoginButtonContainer.js
+++ src/user/ui/loginbutton/LoginButtonContainer.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux'
 import LoginButton from './LoginButton'
-import { loginUser } from './LoginButtonActions'
+// import { loginUser } from './LoginButtonActions'
+import { default as contract } from 'truffle-contract'
+import store from "../../../store"
+
+import fundcoin_contract from "../../../../build/contracts/Migrations.json";
+const Fundcoin = contract(fundcoin_contract);
 
 const mapStateToProps = (state, ownProps) => {
   return {}
@@ -9,10 +14,28 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onLoginUserClick: (event) => {
-      event.preventDefault();
+      debugger;
+      const web3 = store.getState().web3.web3Instance
+      Fundcoin.setProvider(web3.currentProvider);
+      web3.eth.getAccounts(function(err, accs) {
+        if (err != null) {
+          alert("There was an error fetching your accounts.");
+          return;
+        }
 
-      dispatch(loginUser())
-    }
+        if (accs.length == 0) {
+          alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+          return;
+        }
+
+        console.log(accs);
+
+        // accounts = accs;
+        // account = accounts[0];
+        //
+        // self.refreshBalance();
+      });
+    },
   }
 }
 

5204e79e32de3a1034e18a2a8cf991070b4916bc
br0s <dima@fundbox.com>
2017-10-25 17:37:53 +0300

    FundboxContract [WIP]

---
+++ contracts/FundboxContract.sol
@@ -0,0 +1,91 @@
+pragma solidity ^0.4.11;
+
+
+contract FundboxContract {
+
+    enum LoanRequestStatus { Accepted, Rejected, Pending }
+
+    struct LoanRequest {
+        uint amount;
+        LoanRequestStatus status;
+        uint block_number;
+
+    }
+
+    enum ExtendedLoanStatus { Open, Repaid, Delq }
+
+    struct ExtendedLoan {
+        uint amount_extended;
+        uint fees;
+        uint balance;
+        uint block_number_due;
+        ExtendedLoanStatus status;
+    }
+
+    struct AddressCreditInfo {
+        bool exists;
+        uint credit_limit;
+        uint credit_available;
+    }
+
+    struct AddressLoanRequests {
+        LoanRequest[] requests;
+    }
+
+    struct AddressExtendedLoans {
+        ExtendedLoan[] loans;
+    }
+
+    uint public funds_available;
+
+
+    mapping(address => AddressLoanRequests) loan_requests;
+    mapping(address => AddressExtendedLoans) extended_loans;
+    mapping(address => AddressCreditInfo) credit_infos;
+
+    function fundContract(uint x) public {
+        funds_available += x;
+
+    }
+
+    function getFundsAvailable() public returns (uint) {
+        return funds_available;
+    }
+
+    function loanExtendedUpdateCredit(uint amount) {
+        if (!credit_infos[msg.sender].exists) {
+            credit_infos[msg.sender].exists = true;
+            credit_infos[msg.sender].credit_limit = 1000;
+            credit_infos[msg.sender].credit_available = 1000 - amount;
+        } else {
+            credit_infos[msg.sender].credit_available -= amount;
+        }
+    }
+
+    function repaymentMadeUpdateCredit(uint amount) {
+        credit_infos[msg.sender].credit_available += amount;
+    }
+
+
+    function requestLoan(uint amount) public returns (bool) {
+        loan_requests[msg.sender].requests.push(LoanRequest({amount : amount, status : LoanRequestStatus.Accepted, block_number : block.number}));
+        extended_loans[msg.sender].loans.push(ExtendedLoan({amount_extended : amount, fees : 0,
+                                    balance : amount,  block_number_due : 0, status : ExtendedLoanStatus.Open}));
+        loanExtendedUpdateCredit(amount);
+        return true;
+
+    }
+
+    function getAvailableCredit() public returns (uint) {
+        return credit_infos[msg.sender].credit_available;
+    }
+
+    function makePayment(uint amount) public returns (bool) {
+        for (uint i = 0; i < extended_loans[msg.sender].loans.length(); i++) {
+            if (extended_loans[msg.sender].loans[i].balance > 0) {
+                extended_loans[msg.sender].loans[i].balance -= amount;
+                return true;
+            }
+        }
+    }
+}
---
+++ test/TestFundboxContract.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.11;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/FundboxContract.sol";
+
+contract TestFundboxContract {
+    FundboxContract fundbox_contract = FundboxContract(DeployedAddresses.FundboxContract());
+
+    /*function testGetFundsAvailable() {
+        fundbox_contract.fundContract(20000);
+        uint returned = fundbox_contract.getFundsAvailable();
+        Assert.equal(returned, 20000, "bla bla");
+    }*/
+
+    function testRequestLoan() {
+        bool b = fundbox_contract.requestLoan(100);
+        b = fundbox_contract.requestLoan(100);
+
+        uint r = fundbox_contract.getAvailableCredit();
+
+        Assert.equal(r, 800, "bla bla");
+    }
+
+    function testMakePayment() {
+        bool b = fundbox_contract.requestLoan(100);
+        b = fundbox_contract.makePayment(50);
+
+        uint r = fundbox_contract.getAvailableCredit();
+
+        Assert.equal(r, 950, "bla bla");
+    }
+}

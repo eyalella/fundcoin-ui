74672668b1a2b7e9f324f46ae78aad27507328a3
Eyal Ella <eyal.ella@fundbox.com>
2017-10-25 21:15:15 +0300

    some UI

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -5,7 +5,8 @@ class Home extends Component {
   render() {
     return(
       <main className="container">
-        <h1>Please enter your wallet address</h1>
+        <img src="https://user-images.githubusercontent.com/3458336/32014977-54fb29d4-b9c8-11e7-9144-366294d24335.png" />
+        <h1>Hello.</h1>
         <AddAddressButtonContainer></AddAddressButtonContainer>
       </main>
     )

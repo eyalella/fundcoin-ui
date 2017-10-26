108b1d55732dec2f4b5e8d6699e26be9125df359
br0s <dima@fundbox.com>
2017-10-26 03:05:44 +0300

    FundboxContract [WIP]

--- test/TestFundboxContract.sol
+++ test/TestFundboxContract.sol
@@ -7,6 +7,14 @@ import "../contracts/FundboxContract.sol";
 contract TestFundboxContract {
     FundboxContract fundbox_contract = FundboxContract(DeployedAddresses.FundboxContract());
 
+    uint public initialBalance = 10 ether;
+
+    function testShit() {
+        fundbox_contract.investInFundCoin.value(2000)();
+        uint bl = fundbox_contract.balanceOf(msg.sender);
+        Assert.equal(bl, 98, "bla bla");
+    }
+
     function testGetUserData() {
         var (a,b,c,d) = fundbox_contract.getUserData();
         Assert.equal(d, 0, "bla bla");
